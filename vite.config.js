8d6c85d477d3ec0219d5fdc1038b9e05b898a086
김익현 <69383887+dlrgus041@users.noreply.github.com>
2026-03-20 14:26:14 +0900

    Vercel 배포 설정을 위한 vite.config.js 수정
    base 프로퍼티 주석 처리

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AIBE4_FinalProject_Team2_FE/',
+  // base: '/AIBE4_FinalProject_Team2_FE/',
   server: {
     proxy: {'/api/v1': 'http://localhost:8081/api/v1'},
   }
